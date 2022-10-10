a352a110abd4c668b0b3b3eea0a9675e7a5d5dcb
Anatole-DC <adechauveron@gmail.com>
2022-10-10 15:06:09 +0200

    :boom: Update repository

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,23 +1,56 @@
 ﻿using System;
 using System.Diagnostics;
-using Leonardo;
+using System.IO;
+using System.Threading.Tasks;
+using Fibonacci;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-var stopwatch = new Stopwatch();
-
-stopwatch.Start();
-var listOfResults = Fibonacci.RunAsync(args);
-
-foreach (var listOfResult in listOfResults)
+internal class Program
 {
-    Console.WriteLine($"Result : {listOfResult}");
+    private static async Task Main(string[] args)
+    {
+        var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddEnvironmentVariables().AddJsonFile("appsettings.json", false, true)
+            .AddJsonFile($"appsettings.{environmentName}.json", true, true).Build();
+
+
+        var applicationSection = configuration.GetSection("Application");
+        var applicationConfig = applicationSection.Get<ApplicationConfig>();
+
+        var services = new ServiceCollection();
+        services.AddDbContext<FibonacciDataContext>(
+            options => options.UseSqlServer(configuration.GetConnectionString("FiboConnection")));
+        services.AddSingleton<Compute>();
+        services.AddLogging();
+
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        
+        using (var serviceProvider = services.BuildServiceProvider())
+        {
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+            logger.LogInformation($"Application Name : {applicationConfig.Name}");
+            logger.LogInformation($"Application Message : {applicationConfig.Message}");
+            var compute = serviceProvider.GetService<Compute>();
+            var results = await compute.ExecuteAsync(args);
+
+            foreach (var result in results)
+            {
+                logger.LogInformation($"Fibonacci result : {result}");
+            }
+        }
+
+        stopwatch.Stop();
+        Console.Write($"Elapsed seconds: {stopwatch.Elapsed.TotalSeconds}");
+    }
+
+    public class ApplicationConfig
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
 }
-stopwatch.Stop();
-
-Console.WriteLine("time elapsed in seconds : " + stopwatch.Elapsed.Seconds);
-
-// int Fib(int i)
-// {
-//     if (i <= 2) return 1;
-//     return Fib(i - 1) + Fib(i - 2);
-// }  
-
